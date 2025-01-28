51d02efdcf18c177fee7b9c45d3b1caa6b30e545
ozkazanc <alisanozkazanc@gmail.com>
2025-01-28 16:30:02 +0100

    Rendered ambassador painting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+    //import img_src from '$lib/assets/Hans_Holbein-The_Ambassadors.jpg'
+</script>
+
 <h1>Welcome to SvelteKit kiddo</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!--/> 
+<img src={img_src} alt="Ambassadors">
+<-->
+<img src="/Hans_Holbein-The_Ambassadors.jpg" alt="Ambassadors">
+<img src="/favicon.png" alt="Ambassadors">
