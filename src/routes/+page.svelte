ae8531fe0b727116465f9f26126b3b191d4ad5cd
ozkazanc <alisanozkazanc@gmail.com>
2025-01-31 22:19:07 +0100

    Basic fog + clearing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,42 @@
 <script>
     //import img_src from '$lib/assets/Hans_Holbein-The_Ambassadors.jpg'
+    import { onMount } from "svelte";
     let name = $state('world');
     let submittedText = $state('');
 
     function oninputsubmit() {
         if(name === '') return;
         submittedText = name;
+        reveal(submittedText)
         name = '';
     }
+
+    let canvas = $state();
+    let ctx = $state();
+    
+    let wordToCoordinates = {
+        hand: [{ x: 100, y: 200, width: 80, height: 50 }],
+        face: [{ x: 0, y: 0, width: 250, height: 250 }, { x: 250, y: 250, width: 250, height: 250 }]
+    };
+
+    /**
+     * @param {string} word
+     */
+    function reveal(word) {
+        // @ts-ignore
+        if (wordToCoordinates[word]) {
+            // @ts-ignore
+            wordToCoordinates[word].forEach(({ x, y, width, height }) => {
+                ctx.clearRect(x, y, width, height);
+            });
+        }
+    }
+    
+    onMount(() => {
+        ctx = canvas.getContext("2d");
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height); // Full fog overlay
+    });
 </script>
 
 <h1>Welcome to SvelteKit kiddo</h1>
@@ -25,3 +54,30 @@
     <p>{submittedText}</p>
     <script>alert('hey')</script>
 {/if}
+
+<div class="game-container">
+    <img src="/Hans_Holbein-The_Ambassadors.jpg" alt="Hidden Painting" class="painting" />
+    <canvas bind:this={canvas} width="500" height="500"></canvas>
+</div>
+
+<style>
+    .game-container {
+      position: relative;
+      width: 500px; /* Adjust as needed */
+      height: 500px;
+    }
+    .painting {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+    canvas {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+</style>
