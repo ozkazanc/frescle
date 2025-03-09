c95514f0bf43680cef940e0c0ee23479af02de27
ozkazanc <alisanozkazanc@gmail.com>
2025-03-09 20:26:50 +0100

    Inserting  email into db, dupe email+token handled

--- src/lib/db/mongo.ts
+++ src/lib/db/mongo.ts
@@ -12,21 +12,46 @@ export function start_mongo(): Promise<MongoClient> {
 
 export async function subscribe(email: string) {
     if(email === "") {
-        throw new Error("Email cannot be empty.");
+        //throw new Error("Email cannot be empty.");
+        return;
     }
-
-    // Try to Insert
-    console.log(`[MongoDB Insert \"${DB_COLL_NAME}\"]: ${email}`);
-
-    // Check for duplicate
-    const duplicate = await newsletter.findOne({ email: email} );
-    if(duplicate) {
-        //throw new Error(`\"${email}\" already exists.`);
-        return; // Fail silently: don't add but don't raise an error
-    }
-
-    // Create a token for unsubscription
-
 
     // Insert
+    console.log(`[MongoDB Insert \"${DB_COLL_NAME}\"]: ${email}`);
+
+    // Rely on db response to see email/token duplicates
+    let inserted: boolean = false;
+    while(!inserted) {
+        try {
+            const result = await newsletter.insertOne({
+                email: email,
+                subscribed: true,
+                created_at: new Date(Date.now()).toISOString(),
+                token: generateToken()
+            });
+            console.log(result);
+            inserted = result.acknowledged;
+        }
+        catch(e: any) {
+            //console.error(e)
+            if(e.errorResponse.code === 11000 && e.errmsg.includes("index: email_1")) {
+                console.log("Dupe email");
+                inserted = true; // Silently accept dupe email, don't show error
+            }
+            else if(e.errorResponse.code === 11000 && e.errmsg.includes("index: token_1")) {
+                console.log("Dupe token");
+            }
+        }
+    }
+}
+
+function generateToken(): string {
+    const CHARS: string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const TOKEN_LENGTH = 32;
+    let token: string = "";
+    
+    for(let i = 0; i < TOKEN_LENGTH; i++){
+        token += CHARS[Math.floor(Math.random() * CHARS.length)];
+    }
+    return token;
 }
